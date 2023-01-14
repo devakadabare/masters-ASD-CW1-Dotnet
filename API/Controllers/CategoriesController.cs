8576560fa436bd4fcb00143114cc6132a5156e3f
Devaka <devaka.dabare@immunify.me>
2023-01-15 00:03:46 +0530

    clean

--- API/Controllers/CategoriesController.cs
+++ API/Controllers/CategoriesController.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using API.Data;
+
 using API.DTO;
 using API.Entities;
 using API.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+
 
 namespace API.Controllers
 {
--- API/Services/AccountService.cs
+++ API/Services/AccountService.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Data;
 using API.DTO;
 using API.Entities;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Services
